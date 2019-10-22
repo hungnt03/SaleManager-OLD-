fc9b034f486adc9ef59a0dde8426f3763dbcf32e
hungnt home <hungnt.hut56@gmail.com>
2019-10-23 00:08:12 +0900

    Create Default view

---
+++ SaleManager.WPF/Views/Default/DefaultView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SaleManager.WPF.Views.Default
+{
+    /// <summary>
+    /// Interaction logic for SaleView.xaml
+    /// </summary>
+    public partial class DetaultView : UserControl
+    {
+        public DetaultView()
+        {
+            InitializeComponent();
+        }
+    }
+}
